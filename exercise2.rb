ca06499ac44db4384a514d2aba71bafc7b545ba1
“hcm90” <heitor.moura@gmail.com>
2014-01-14 13:10:31 -0500

    RubyIntro1

---
+++ exercise2.rb
@@ -0,0 +1,8 @@
+meal = 55
+tip = 1.15
+string = "string"
+variable = 1
+puts "#{meal * tip}"
+puts "#{string + variable.to_s}"
+puts "#{45628*7839}"
+puts (true && false) || (false && true) || !(false && false)
---
+++ exercise3.rb
@@ -0,0 +1,8 @@
+puts "What is your name?"
+name = gets.chomp
+puts "Hi #{name}!"
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "What year were you born in?"
+birthyear = gets.chomp.to_i
+puts "Okay. Your name is #{name}, you are #{age} years old, and you were born in #{birthyear}."
